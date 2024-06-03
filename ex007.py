5630b9843be04f4f921be4a569b201559699f036
Paulo Roberto <dante.hill.o@proton.me>
2024-06-03 04:06:12 -0300

    007 done

--- ex007.py
+++ ex007.py
@@ -1,1 +1,6 @@
 #  Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32
+
+temperatura = int(input('Temperatuda em Celsius: '))
+calc_fahrenheit = temperatura * 9 / 5 + 32
+
+print(f'{calc_fahrenheit:.2f}')
