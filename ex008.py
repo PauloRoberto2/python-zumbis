e73f1213069b3defbae177aefd90274cd602a5b7
Paulo Roberto <dante.hill.o@proton.me>
2024-06-03 04:37:58 -0300

    008 done

--- ex008.py
+++ ex008.py
@@ -1,1 +1,6 @@
 # Faça agora o contrário, de Fahrenheit para Celsius.
+
+temperatura = int(input('Temperatuda em Fahrenheit: '))
+calc_celsius = (temperatura - 32) * 5 / 9
+
+print(f'{calc_celsius:.2f}')
