7ae88509a92b1706bf562d1dc07326d83722c2a5
Paulo Roberto <dante.hill.o@proton.me>
2024-06-03 01:48:46 -0300

    006 done

--- ex006.py
+++ ex006.py
@@ -1,4 +1,7 @@
 # Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 
+distancia = float(input('Distância da viagem: '))
+velocidade_media = float(input('Velocidade média: '))
+tempo = distancia / velocidade_media
 
-
+print(f'O tempo de duração da viagem será: {tempo:.2f}')
