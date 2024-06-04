ab7337b7cc73db17b08191e06954eb6fc1bdb16e
Paulo Roberto <dante.hill.o@proton.me>
2024-06-04 07:13:31 -0300

    no reason to exist

--- teste.py
+++
@@ -1,8 +0,0 @@
-for n in range(0, 180):
-    for x in range(0, n):
-        if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
-            break
-    else:
-        # loop fell through without finding a factor
-        print(n, 'is a prime number')
