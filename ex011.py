f180d38f78ca8bd106aa2432cccfee333a2fc456
Paulo Roberto <dante.hill.o@proton.me>
2024-06-04 07:12:04 -0300

    logica correta, mas com erro

--- ex011.py
+++ ex011.py
@@ -1,1 +1,4 @@
 # Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado a um milhão.
+
+# Da erro: exceeds the limit for integer string conversion;
+print(len(str(2 ** 100000)))
