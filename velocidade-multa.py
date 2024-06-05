440776ff1afe43bced751c6fd7357f85325fca33
Paulo Roberto <dante.hill.o@proton.me>
2024-06-04 21:37:36 -0300

    feito sem problemas

---
+++ velocidade-multa.py
@@ -0,0 +1,12 @@
+velocidade = int(input('Velocidade: '))
+multa = (velocidade - 110) * 5
+
+if velocidade > 110:
+    print('~~~~~~~~~~~~~~~~~~~~')
+    print('MULTADO!')
+    print(f'Valor: R$ {multa:.2f}')
+    print('~~~~~~~~~~~~~~~~~~~~')
+else:
+    print('~~~~~~~~~~~~~~~~~~~~')
+    print('Não foi multado.')
+    print('~~~~~~~~~~~~~~~~~~~~')
