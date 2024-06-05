9fca848042b4050365eca4fb9c2a15968bb09864
Paulo Roberto <dante.hill.o@proton.me>
2024-06-04 23:43:25 -0300

    solved with promotion challenger

--- estruturas-aninhadas.py
+++ estruturas-aninhadas.py
@@ -1,5 +1,6 @@
 minutos = int(input('Minutos: '))
 
+"""
 if minutos < 200:
     valor = minutos * 0.20
 if minutos >= 200 and minutos <= 400:
@@ -8,17 +9,16 @@ if minutos > 400:
     valor = minutos * 0.15
 
 print(f'Total a pagar: R$ {valor:.2f}')
-
-
 """
-# resolução professor:
 
 if minutos < 200:
     valor = 0.20
+elif minutos <= 400:
+    valor = 0.18
+elif minutos <= 800:
+    valor = 0.15
+# Promoção
 else:
-    if minutos <= 400:
-        valor = 0.18
-    else:
-        valor = 0.15
+    valor = 0.08
 
-"""
+print(f'R$ {minutos * valor:.2f}')
