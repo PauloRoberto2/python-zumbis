1245b5aee765c2600bf18078ec50368629c4a6d3
Paulo Roberto <dante.hill.o@proton.me>
2024-06-08 02:17:57 -0300

    Added:exemple of a other way to resolution

--- exercicios-02/ex002.py
+++ exercicios-02/ex002.py
@@ -6,3 +6,10 @@ elif ano % 4 == 0 and ano % 100 != 0:
     print('Ano bissexto!')
 else:
     print('Não é bissexto!')
+
+"""
+trexo da lib calendar.py:
+def isleap(year):
+    """Return True for leap years, False for non-leap years."""
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+"""
