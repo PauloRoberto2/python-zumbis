61f1c097443b2e882f058c776aa77cbf1d79418a
Paulo Roberto <dante.hill.o@proton.me>
2024-06-08 02:32:35 -0300

    added: if the weight is lower

--- exercicios-02/ex003.py
+++ exercicios-02/ex003.py
@@ -1,7 +1,13 @@
 peso_peixe = float(input('Peso do peixe: '))
+print('~' * 30)
 
 # Multa de R$4,00 para cada quilograma a mais que o limite; 50 quilos
-multa = (peso_peixe - 50) * 4
-print('~' * 30)
-print(f'Peso excedente: {peso_peixe - 50:.2f}')
-print(f'Valor da multa: R$ {multa:.2f}')
+
+if peso_peixe > 50:
+    multa = (peso_peixe - 50) * 4
+    print(f'Peso excedente: {peso_peixe - 50:.2f}')
+    print(f'Valor da multa: R$ {multa:.2f}')
+else:
+    multa = excesso = 0
+    print('Peso ok.\nNada a pagar.')
+
