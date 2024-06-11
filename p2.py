b4ff0c029918c421e4ad39fd7a82b6e9a66d8b59
Paulo Roberto <dante.hill.o@proton.me>
2024-06-11 14:30:03 -0300

    remoção
    não fazia sentido manter no repo

--- p2.py
+++
@@ -1,3 +0,0 @@
-n1 = float(input('N1: '))
-n2 = float(input('N2: '))
-print (n1 + n2)
--- programa.py
+++
@@ -1,4 +0,0 @@
-resposta = 42
-print (f'tudo tem {resposta} como solução')
-preço = 9.6666
-print (f'Preço é R$ {preço:.2f}')
